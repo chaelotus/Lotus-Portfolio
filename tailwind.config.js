d22945e26869f61dd80668f7f04a8bb117f5bdec
parkchaeyeon <pcy6904@naver.com>
2023-12-18 23:41:06 +0900

    css body설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'background': '#F5F5F7',
-      'black': '#000000',
-      'pointGray':'#666666',
-      'gray': '#9E9E9E',
-      'lineGray': '#C6C6C6',
-      'pointOrange': '#F1A33B',
-      'porintGreen':'#45C96A',
-      'porintBlue': '#0A84FF'
+    theme: {
+      colors:{
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'background': '#F5F5F7',
+        'black': '#000000',
+        'pointGray':'#666666',
+        'gray': '#9E9E9E',
+        'lineGray': '#C6C6C6',
+        'pointOrange': '#F1A33B',
+        'porintGreen':'#45C96A',
+        'porintBlue': '#0A84FF'
+      }
   },
   plugins: [],
 }
