f28c6c25fe2cd1b5ca5abc05299c685398a54504
parkchaeyeon <pcy6904@naver.com>
2023-12-18 23:07:23 +0900

    기본 색 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    theme: {
-      colors:{
-        transparent: 'transparent',
-        current: 'currentColor',
-        'white': '#ffffff',
-        'background': '#F5F5F7',
-        'black': '#000000',
-        'pointGray':'#666666',
-        'gray': '#9E9E9E',
-        'lineGray': '#C6C6C6',
-        'pointOrange': '#F1A33B',
-        'porintGreen':'#45C96A',
-        'porintBlue': '#0A84FF'
-      }
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'background': '#F5F5F7',
+      'black': '#000000',
+      'pointGray':'#666666',
+      'gray': '#9E9E9E',
+      'lineGray': '#C6C6C6',
+      'pointOrange': '#F1A33B',
+      'porintGreen':'#45C96A',
+      'porintBlue': '#0A84FF'
   },
   plugins: [],
 }
