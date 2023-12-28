2de4494fbdbc6e7e7686567254cc066cedae6251
parkchaeyeon <pcy6904@naver.com>
2023-12-28 14:48:54 +0900

    📝 headerNavList data 추가

---
+++ src/pages/Header/constants.ts
@@ -0,0 +1,18 @@
+export const headerNavList = [
+	{
+		id: 'about',
+		title: 'ABOUT',
+	},
+	{
+		id: 'skills',
+		title: 'SKILLS',
+	},
+	{
+		id: 'project',
+		title: 'PROJECT',
+	},
+	{
+		id: 'contact',
+		title: 'CONTACT',
+	},
+];
