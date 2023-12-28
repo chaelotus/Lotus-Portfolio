abba282c21623c2199a7b19f708ae6f5084a675a
parkchaeyeon <pcy6904@naver.com>
2023-12-28 16:28:47 +0900

    🎨 Lotus page 구조잡기

---
+++ src/pages/Lotus/views/Lotus.tsx
@@ -0,0 +1,12 @@
+const Lotus = () => {
+	return (
+		<>
+			<article className="flex justify-between items-center pt-[110px]">
+				<section>텍스트</section>
+				<section>이미지</section>
+			</article>
+			<article></article>
+		</>
+	);
+};
+export default Lotus;
