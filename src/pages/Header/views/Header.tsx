10d05a9df44f278a8174d8ed84f4deca3097fb5e
parkchaeyeon <pcy6904@naver.com>
2023-12-28 15:14:40 +0900

    🎨 헤더 구조 변경, 버튼 컴포넌트 사용

--- src/pages/Header/views/Header.tsx
+++ src/pages/Header/views/Header.tsx
@@ -1,19 +1,24 @@
 import LogoImage from '../../../assets/LogoImage.svg';
 import light from '../../../assets/light.svg';
+import { headerNavList } from '../constants';
+import Button from '../../../common/components/Button';
 
 const Header = () => {
 	return (
-		<header className="pt-4 pb-4 pr-8 pl-8">
-			<div className="flex justify-between items-center">
-				<div className="w-20 cursor-pointer">
-					<img src={LogoImage} alt="logo" />
-				</div>
-				<div className="flex justify-between items-center">
-					<div className="w-14 cursor-pointer">
-						<img src={light} alt="light" />
-					</div>
-				</div>
+		<header className="pt-4 pb-4 pr-8 pl-8 fixed w-full flex justify-between items-center">
+			<div className="w-20 cursor-pointer">
+				<img src={LogoImage} alt="logo" />
 			</div>
+			<ul className="flex justify-between items-center">
+				{headerNavList.map((item) => (
+					<li>
+						<Button children={item.title} key={item.id} />
+					</li>
+				))}
+				<li className="w-14 cursor-pointer">
+					<img src={light} alt="light" />
+				</li>
+			</ul>
 		</header>
 	);
 };
