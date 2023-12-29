041c629a6a8113b42289db7fa0d61164023c187e
parkchaeyeon <pcy6904@naver.com>
2023-12-29 14:12:28 +0900

    📝 Lotus page text

---
+++ src/pages/Lotus/constants.ts
@@ -0,0 +1,10 @@
+export const LotusPageMainText = {
+	text: [
+		'FRONTEND DEVELOPER',
+		'안녕하세요. 기술발전에 끊임없이 욕심내는',
+		'프론트엔드 개발자',
+		'박채연',
+		'입니다.',
+	],
+};
+export const LernMoreText = 'Learn More';
