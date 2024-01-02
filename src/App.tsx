68286b74f04067c5802de934e498b110781855e9
parkchaeyeon <pcy6904@naver.com>
2024-01-02 15:11:21 +0900

    📝 About Page text 상수화

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import Header from './pages/Header/views/Header';
 import Lotus from './pages/Lotus/views/Lotus';
+import About from './pages/About/views/About';
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 			<Header />
 			<Container maxWidth="lg">
 				<Lotus />
+				<About />
 			</Container>
 		</BrowserRouter>
 	);
---
+++ src/pages/About/constants.ts
@@ -0,0 +1,13 @@
+export const AboutTitle = 'ABOUT ME';
+export const AboutFirstPageIntro = {
+	text: ['안녕하세요. 신입 프론트엔드 개발자 박채연입니다.', '저는'],
+};
+export const AboutFirstPageIntro2 = {
+	text: [
+		'ReactQuery, Styled-Components 같은 신기술을 공부하고 적용합니다.',
+		'React Query로 캐싱에 대한 이해를 바탕으로 백엔드 call 수를 줄이려고 노력합니다.',
+		'Redux를 활용한 상태관리로 re-rendering 방지를 지향하는 설계를 합니다.',
+		'프로젝트 진행시 커뮤니케이션을 위한 문서 작성 후 최상의 Quality를 이끌어냅니다.',
+		'정보처리기사 자격증을 보유하고 있습니다.',
+	],
+};
