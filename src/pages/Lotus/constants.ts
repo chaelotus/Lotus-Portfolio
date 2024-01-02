8b780b0aecaf1935a5c364ea5318541c5d5681fe
parkchaeyeon <pcy6904@naver.com>
2024-01-02 10:34:06 +0900

    💄 Lotus page font 설정

--- src/pages/Lotus/constants.ts
+++ src/pages/Lotus/constants.ts
@@ -4,7 +4,7 @@ export const LotusPageMainText: LotusPageMainTextType = {
 	text: [
 		'FRONTEND DEVELOPER',
 		'안녕하세요. 기술발전에 끊임없이 욕심내는',
-		'프론트엔드 개발자',
+		'프론트엔드개발자 ',
 		'박채연',
 		'입니다.',
 	],
--- src/pages/Lotus/views/Lotus.tsx
+++ src/pages/Lotus/views/Lotus.tsx
@@ -1,11 +1,33 @@
+import { PiCaretDownThin } from 'react-icons/pi';
+import { LernMoreText, LotusPageMainText } from '../constants';
+
 const Lotus = () => {
 	return (
 		<>
-			<article className="flex justify-between items-center pt-[110px]">
-				<section>텍스트</section>
+			<article className="flex justify-between items-center pt-[110px] h-[43.125rem]">
+				<section>
+					<section className="text-6xl font-PretendardMedium mb-2">
+						{LotusPageMainText.text[0]}
+					</section>
+					<section className="text-3xl font-koreaMachineLight">
+						{LotusPageMainText.text[1]}
+					</section>
+					<section className="text-3xl font-koreaMachineLight">
+						<span>{LotusPageMainText.text[2]}</span>
+						<span className="text-pointOrange">
+							{LotusPageMainText.text[3]}
+						</span>
+						<span>{LotusPageMainText.text[4]}</span>
+					</section>
+				</section>
 				<section>이미지</section>
 			</article>
-			<article></article>
+			<article>
+				<section>{LernMoreText}</section>
+				<section>
+					<PiCaretDownThin />
+				</section>
+			</article>
 		</>
 	);
 };
