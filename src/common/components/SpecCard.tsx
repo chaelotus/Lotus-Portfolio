48aa85cc24ff1160a51cfd45f3abf91ccf6207b7
parkchaeyeon <pcy6904@naver.com>
2024-01-04 15:24:15 +0900

    🎨 SpecCard 공통컴포넌트

---
+++ src/common/components/SpecCard.tsx
@@ -0,0 +1,53 @@
+import { TfiWidgetAlt } from 'react-icons/tfi';
+import { AboutSecondPageDetailType } from '../../pages/About/type';
+
+type SpecCardProps<T extends keyof AboutSecondPageDetailType> = {
+	svgFile: string;
+	sectionType: T;
+	dataObject: AboutSecondPageDetailType[T];
+};
+
+const SpecCard = <T extends keyof AboutSecondPageDetailType>({
+	svgFile,
+	sectionType,
+	dataObject,
+}: SpecCardProps<T>) => {
+	const sectionData = extractSectionData(sectionType, dataObject);
+	return (
+		<div className="flex justify-between items-center bg-white rounded-xl p-5 font-NanumSquareNeoBold">
+			<section className="w-[15%]">
+				<img className="w-[95px]" src={svgFile} />
+			</section>
+			<section className="flex-col w-[25%]">
+				<div className="font-bold text-xl mb-2">{sectionData.agency}</div>
+				<div className="text-pointGray text-xs font-bold">
+					{sectionData.date}
+				</div>
+			</section>
+			<section className="w-[60%]">
+				<h1 className="text-xl pb-3 text-pointOrange">
+					{sectionData.subtitle}
+				</h1>
+				<ul className="list-disc text-pointGray">
+					{sectionData.detail.map((text: string, index: number) => {
+						return (
+							<div className="flex items-center">
+								<TfiWidgetAlt />
+								<li className="pl-2" key={index}>
+									{text}
+								</li>
+							</div>
+						);
+					})}
+				</ul>
+			</section>
+		</div>
+	);
+};
+function extractSectionData<T extends keyof AboutSecondPageDetailType>(
+	sectionType: T,
+	dataObject: AboutSecondPageDetailType[T],
+): AboutSecondPageDetailType[T] {
+	return dataObject;
+}
+export default SpecCard;
