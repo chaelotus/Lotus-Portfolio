492de12e17d4be0f7dabd791ef5b1af99e0975ca
parkchaeyeon <pcy6904@naver.com>
2024-01-06 00:04:12 +0900

    📝 skills data 상수화

---
+++ src/pages/Skills/constants.ts
@@ -0,0 +1,49 @@
+import { SkillsDetailType } from './type';
+import skill_react from '../../assets/skill_react.svg';
+import skill_ts from '../../assets/skill_ts.svg';
+import skill_rq from '../../assets/skill_rq.svg';
+import skill_redux from '../../assets/skill_redux.svg';
+import skill_sc from '../../assets/skill_sc.svg';
+import skill_tw from '../../assets/skill_tw.svg';
+import skill_js from '../../assets/skill_js.svg';
+import skill_html from '../../assets/skill_html.svg';
+import skill_css from '../../assets/skill_css.svg';
+export const SkillsTitle = 'SKILLS';
+export const SkillsDetail: SkillsDetailType = [
+	{
+		svg: skill_react,
+		title: 'React',
+	},
+	{
+		svg: skill_ts,
+		title: 'TypeScript',
+	},
+	{
+		svg: skill_rq,
+		title: 'React Query',
+	},
+	{
+		svg: skill_redux,
+		title: 'Redux',
+	},
+	{
+		svg: skill_sc,
+		title: 'Styled Components',
+	},
+	{
+		svg: skill_tw,
+		title: 'Tailwind',
+	},
+	{
+		svg: skill_js,
+		title: 'JavaScript',
+	},
+	{
+		svg: skill_html,
+		title: 'HTML',
+	},
+	{
+		svg: skill_css,
+		title: 'CSS',
+	},
+];
