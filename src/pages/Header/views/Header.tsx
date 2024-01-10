5cf1f8bec14afddb1733f1411b61fe06e0fbb2d1
parkchaeyeon <pcy6904@naver.com>
2024-01-10 14:45:33 +0900

    ⚡️ 모달이 뜰 때는 헤더 보이지 않게 구현

--- src/pages/Header/views/Header.tsx
+++ src/pages/Header/views/Header.tsx
@@ -1,25 +1,31 @@
+import { useSelector } from 'react-redux';
 import LogoImage from '../../../assets/LogoImage.svg';
 import light from '../../../assets/light.svg';
 import { headerNavList } from '../constants';
 import Button from '../../../common/components/Button';
+import { RootState } from '../../../common/store/Store';
 
 const Header = () => {
-	return (
-		<header className="pt-4 pb-4 pr-8 pl-8 fixed w-full flex justify-between items-center z-10 header">
-			<div className="w-20 cursor-pointer">
-				<img src={LogoImage} alt="logo" />
-			</div>
-			<ul className="flex justify-between items-center">
-				{headerNavList.map((item) => (
-					<li>
-						<Button children={item.title} key={item.id} />
+	const isOpen = useSelector((state: RootState) => state.ModalReducer.isOpen);
+
+	if (!isOpen) {
+		return (
+			<header className="pt-4 pb-4 pr-8 pl-8 fixed w-full flex justify-between items-center z-10 header">
+				<div className="w-20 cursor-pointer">
+					<img src={LogoImage} alt="logo" />
+				</div>
+				<ul className="flex justify-between items-center">
+					{headerNavList.map((item) => (
+						<li key={item.id}>
+							<Button children={item.title} />
+						</li>
+					))}
+					<li className="w-14 cursor-pointer">
+						<img src={light} alt="light" />
 					</li>
-				))}
-				<li className="w-14 cursor-pointer">
-					<img src={light} alt="light" />
-				</li>
-			</ul>
-		</header>
-	);
+				</ul>
+			</header>
+		);
+	}
 };
 export default Header;
