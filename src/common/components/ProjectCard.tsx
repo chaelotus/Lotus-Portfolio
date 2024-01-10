dc4118ce9f339a9d498986089a0715de8a71ec18
parkchaeyeon <pcy6904@naver.com>
2024-01-10 14:45:15 +0900

    ⚡️ projectCard 클릭하면 모달 뜨게 구현

--- src/common/components/ProjectCard.tsx
+++ src/common/components/ProjectCard.tsx
@@ -1,4 +1,8 @@
+import { useSelector, useDispatch } from 'react-redux';
 import ClickButton from './ClickButton';
+import { RootState } from '../store/Store';
+import { openModal } from '../reducers/IsOpen';
+import { useEffect } from 'react';
 const ProjectCard = ({
 	svgFile,
 	projectTitle,
@@ -6,15 +10,26 @@ const ProjectCard = ({
 	svgFile: string;
 	projectTitle: string;
 }) => {
+	const isOpen = useSelector((state: RootState) => state.ModalReducer.isOpen);
+	const dispatch = useDispatch();
+	const handleClick = () => {
+		dispatch(openModal());
+	};
+	useEffect(() => {
+		console.log(isOpen);
+	}, [isOpen]);
 	return (
-		<div className="flex flex-col w-full h-[455px] bg-white rounded-xl cursor-pointer ml-6 group">
+		<div
+			onClick={handleClick}
+			className="flex flex-col w-full h-[455px] bg-white rounded-xl cursor-pointer ml-6 group"
+		>
 			<div className=" h-[85%] rounded-t-xl overflow-hidden group-hover:scale-95 group-hover:rounded-xl transition-transform">
 				<img className="w-full" src={svgFile} />
 			</div>
 
 			<div className="h-[15%] flex justify-between items-center bg-white p-5 rounded-b-xl">
 				<div className="text-xl">{projectTitle}</div>
-				<div>
+				<div onClick={handleClick}>
 					<ClickButton />
 				</div>
 			</div>
