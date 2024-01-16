5f434710e0102b5dd7b8106f5aa96dc0f14e9eb1
parkchaeyeon <pcy6904@naver.com>
2024-01-16 21:34:35 +0900

    🎨 darkMode redux code 수정

--- src/common/reducers/DarkMode.ts
+++ src/common/reducers/DarkMode.ts
@@ -8,7 +8,12 @@ export const lightMode = () => ({ type: LIGHT_MODE });
 
 type DarkModeState = { isDark: boolean };
 // 초기 상태
-export const initialState: DarkModeState = { isDark: false };
+export const initialState: DarkModeState = {
+	isDark:
+		localStorage.theme === 'dark' ||
+		(!('theme' in localStorage) &&
+			window.matchMedia('(prefers-color-scheme: dark)').matches),
+};
 
 type DarkModeAction =
 	| ReturnType<typeof darkMode>
@@ -21,9 +26,11 @@ export const DarkModeReducer = (
 ) => {
 	switch (action.type) {
 		case DARK_MODE:
-			return { isDark: true };
-		case LIGHT_MODE:
+			localStorage.theme = 'dark';
 			return { isDark: false };
+		case LIGHT_MODE:
+			localStorage.theme = 'light';
+			return { isDark: true };
 		default:
 			return state;
 	}
