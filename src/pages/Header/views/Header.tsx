70760e711dbaa2bf5f9405f99fa4212cc2d2ef5c
parkchaeyeon <pcy6904@naver.com>
2024-01-16 16:39:14 +0900

    ⚡️ 이미지 클릭 시 이미지 변경

--- src/pages/Header/views/Header.tsx
+++ src/pages/Header/views/Header.tsx
@@ -1,13 +1,30 @@
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import LogoImage from '../../../assets/LogoImage.svg';
 import light from '../../../assets/light.svg';
+import dark from '../../../assets/dark.svg';
 import { headerNavList } from '../constants';
 import Button from '../../../common/components/Button';
 import { RootState } from '../../../common/store/Store';
 import { Link } from 'react-scroll';
+import { darkMode, lightMode } from '../../../common/reducers/DarkMode';
 
 const Header = () => {
+	const [LightImageIsClick, setLightImageIsClick] = useState(false);
 	const isOpen = useSelector((state: RootState) => state.ModalReducer.isOpen);
+	const isDark = useSelector(
+		(state: RootState) => state.DarkModeReducer.isDark,
+	);
+	const dispatch = useDispatch();
+
+	const handleLightImage = () => {
+		setLightImageIsClick(!LightImageIsClick);
+		if (LightImageIsClick) {
+			dispatch(darkMode());
+		} else {
+			dispatch(lightMode());
+		}
+	};
 
 	if (!isOpen) {
 		return (
@@ -33,8 +50,15 @@ const Header = () => {
 							</li>
 						</Link>
 					))}
-					<li className="w-14 cursor-pointer">
-						<img src={light} alt="light" />
+					<li
+						onClick={handleLightImage}
+						className="w-14 cursor-pointer items-center"
+					>
+						{isDark ? (
+							<img src={dark} alt="dark" />
+						) : (
+							<img src={light} alt="light" />
+						)}
 					</li>
 				</ul>
 			</header>
