9bf8313ffc1de542da81f4493c759a1c38e3be5d
parkchaeyeon <pcy6904@naver.com>
2024-01-18 14:03:41 +0900

    💄 화면 크기 지정

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {
+			screens: {
+				sm: { min: '391px', max: '767px' },
+				md: { min: '768px', max: '1023px' },
+				lg: { min: '1024px' },
+			},
 			colors: {
 				transparent: 'transparent',
 				current: 'currentColor',
