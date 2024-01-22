bd2f3cfdb1ec983793856fbfef84b9f0e0a77652
parkchaeyeon <pcy6904@naver.com>
2024-01-22 15:28:23 +0900

    💄 header button hover 효과 설정

--- src/common/components/Button.tsx
+++ src/common/components/Button.tsx
@@ -14,7 +14,7 @@ const Button = ({
 	};
 	return (
 		<button
-			className="bg-white rounded-md px-5 py-2 flex justify-center items-center mr-10 sm:mr-0 mobile:mr-0 shadow-2xl font-PretendardSemiBold md:text-sm sm:bg-background sm:shadow-none sm:text-lg mobile:bg-background mobile:shadow-none mobile:text-lg"
+			className="bg-white rounded-md px-5 py-2 flex justify-center items-center mr-10 sm:mr-0 mobile:mr-0 shadow-2xl font-PretendardSemiBold md:text-sm sm:bg-background sm:shadow-none sm:text-lg mobile:bg-background mobile:shadow-none mobile:text-lg hover:scale-105 hover:text-pointOrange hover:ease-in-out hover:duration-300 active:text-none"
 			onClick={handleNavBtnClick}
 		>
 			{children}
