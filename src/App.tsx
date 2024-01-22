7a8f6bc5f4442a80417b70a237fb40edf46d36b6
parkchaeyeon <pcy6904@naver.com>
2024-01-22 17:02:35 +0900

    ⚡️ 화면 사이즈 390px 이하일때 경고 화면 띄우기

--- src/App.tsx
+++ src/App.tsx
@@ -14,26 +14,50 @@ import Contact from './pages/Contact/views/Contact';
 import Footer from './pages/Footer/views/Footer';
 import MoveToTop from './common/components/MoveToTop';
 import SmallSizeNav from './common/components/SmallSizeNav';
+import { TbAlertCircleFilled } from 'react-icons/tb';
+import { useEffect, useState } from 'react';
 
 const store = createStore(rootReducer);
 function App() {
+	const [isXsmallSize, setIsXsmallSize] = useState(false);
+	useEffect(() => {
+		const handleResize = () => {
+			setIsXsmallSize(window.innerWidth < 390);
+		};
+		window.addEventListener('resize', handleResize);
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
 	return (
 		<Provider store={store}>
 			<BrowserRouter>
-				<div className="dark:bg-[#222222] overflow-x-hidden">
-					<Header />
-					<MoveToTop />
-					<Container maxWidth="lg">
-						<Lotus />
-						<About />
-						<Skills />
-						<Project />
-						<Contact />
-					</Container>
-					<SmallSizeNav />
-					<DetailModal />
-					<Footer />
-				</div>
+				{isXsmallSize ? (
+					<div className="bg-background w-full h-full absolute flex justify-center items-center">
+						<div className="flex flex-col items-center">
+							<TbAlertCircleFilled className="text-4xl mb-3 text-pointOrange" />
+							<span className="font-NanumSquareNeoBold text-pointGray font-bold">
+								화면을 390px 이상으로 넓혀주세요.
+							</span>
+						</div>
+					</div>
+				) : (
+					<div className="dark:bg-[#222222] overflow-x-hidden">
+						<Header />
+						<MoveToTop />
+						<Container maxWidth="lg">
+							<Lotus />
+							<About />
+							<Skills />
+							<Project />
+							<Contact />
+						</Container>
+						<SmallSizeNav />
+						<DetailModal />
+						<Footer />
+					</div>
+				)}
 			</BrowserRouter>
 		</Provider>
 	);
