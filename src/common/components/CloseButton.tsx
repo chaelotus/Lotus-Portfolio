ad1751733cfcf9d6af574cb17c65a74be3c67f2b
parkchaeyeon <pcy6904@naver.com>
2024-01-23 17:57:33 +0900

    💄 close button 반응형 사이즈

--- src/common/components/CloseButton.tsx
+++ src/common/components/CloseButton.tsx
@@ -12,10 +12,12 @@ const CloseButton = () => {
 			onClick={handleClick}
 			className="flex items-center px-3 py-2 bg-white rounded-lg cursor-pointer shadow-2xl dark:text-black"
 		>
-			<div className="text-2xl mr-1">
+			<div className="text-2xl mr-1 md:text-xl sm:text-xl mobile:text-lg">
 				<RiCloseFill />
 			</div>
-			<span className="text-lg font-PretendardSemiBold">{CloseBtn}</span>
+			<span className="text-lg font-PretendardSemiBold md:text-base sm:text-base mobile:text-sm">
+				{CloseBtn}
+			</span>
 		</div>
 	);
 };
