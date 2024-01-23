8074ca85afbc04c3c21bb8341361b51d8813eba4
parkchaeyeon <pcy6904@naver.com>
2024-01-23 17:59:14 +0900

    💄 detailModal 반응형 사이즈

--- src/pages/Detail/views/DetailModal.tsx
+++ src/pages/Detail/views/DetailModal.tsx
@@ -5,6 +5,7 @@ import CloseButton from '../../../common/components/CloseButton';
 
 const DetailModal = () => {
 	const isOpen = useSelector((state: RootState) => state.ModalReducer.isOpen);
+
 	if (!isOpen) {
 		return null;
 	}
@@ -12,10 +13,11 @@ const DetailModal = () => {
 		isOpen &&
 		createPortal(
 			<div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-backgroundGray z-999 dark:bg-[#222222]">
-				<div className="bg-backgroundGray p-[20px] w-[80%] z-1000 h-full relative dark:bg-[#222222] dark:text-white">
-					<div className="fixed flex right-20">
+				<div className="bg-backgroundGray p-[20px] w-[90%] z-1000 h-full relative dark:bg-[#222222] dark:text-white">
+					<div className="fixed flex right-20 md:right-10 sm:right-10 mobile:right-5">
 						<CloseButton />
 					</div>
+					<div className="pt-20 flex flex-col items-center"></div>
 				</div>
 			</div>,
 
