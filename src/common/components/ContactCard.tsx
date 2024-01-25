5038c911f96e812a4fa014fecaadd96bd67e384f
parkchaeyeon <pcy6904@naver.com>
2024-01-26 02:33:15 +0900

    ⚡️ 🎨 ContactCard 컴포넌트 보완

--- src/common/components/ContactCard.tsx
+++ src/common/components/ContactCard.tsx
@@ -1,19 +1,62 @@
-const ContactCard = () => {
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/Store';
+import { contactCardType } from '../../pages/Contact/type';
+
+type contactCardProps<T extends keyof contactCardType> = {
+	contactObj: contactCardType[T];
+};
+
+const ContactCard = <T extends keyof contactCardType>({
+	contactObj,
+}: contactCardProps<T>) => {
+	const sectionData = extractSectionData(contactObj);
+	const isDark = useSelector(
+		(state: RootState) => state.DarkModeReducer.isDark,
+	);
 	return (
-		<div className="flex flex-col justify-center w-[337px] h-[400px] border-solid border-2 border-indigo-600 md:w-[200px] md:h-[350px] sm:w-[170px] sm:h-[330px] mobile:w-[350px] mobile:h-[130px] mobile:mb-3 mobile:flex-row">
-			{/* <img /> */}
-			<div className="bg-white h-[60%] mobile:w-[40%] mobile:h-full">
-				이미지
+		<div className="flex flex-col justify-center w-[337px] h-[400px] border-solid md:w-[200px] md:h-[350px] sm:w-[170px] sm:h-[330px] mobile:w-[350px] mobile:h-[130px] mobile:mb-14 mobile:flex-row mobile:justify-center">
+			<div className="h-[60%] mobile:w-[40%] mobile:h-full">
+				{isDark ? (
+					<img
+						className="w-full h-full"
+						src={sectionData.mainImageDark}
+						alt=""
+					/>
+				) : (
+					<img className="w-full h-full" src={sectionData.mainImage} alt="" />
+				)}
 			</div>
-			<div className="h-[40%] mobile:w-[60%] mobile:h-full">
-				<span>텍스트</span>
-				<div className="flex">
-					{/* <img /> */}
-					<div className="mr-2">아이콘</div>
-					<span>텍스트</span>
+			<div className="h-[40%] mobile:w-[60%] mobile:h-full flex flex-col justify-center items-center">
+				{sectionData.description.split('\n').map((text, index) => {
+					return (
+						<p
+							className="font-NanumSquareNeoBold text-darkGray dark:text-white md:text-xs md:text-center sm:text-[11px] sm:text-center mobile:text-center"
+							key={index}
+						>
+							{text}
+						</p>
+					);
+				})}
+				<div
+					className="flex items-center mt-10 text-xl cursor-pointer duration-150 transition-all hover:text-pointOrange dark:text-white sm:text-base mobile:text-sm mobile:items-start dark:hover:text-pointOrange"
+					onClick={() => {
+						if (sectionData.url) window.open(`${sectionData.url}`);
+					}}
+				>
+					<div className="mr-3 text-2xl">
+						<sectionData.iconImage />
+					</div>
+					<span className="font-OAGothicExtraMedium">
+						{sectionData.iconTitle}
+					</span>
 				</div>
 			</div>
 		</div>
 	);
 };
+function extractSectionData<T extends keyof contactCardType>(
+	contactObj: contactCardType[T],
+): contactCardType[T] {
+	return contactObj;
+}
 export default ContactCard;
