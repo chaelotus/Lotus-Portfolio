8f8ce91be3ffe89d4b157a6c96461643376a502c
parkchaeyeon <pcy6904@naver.com>
2024-01-26 01:48:41 +0900

    🏷️ type 추가

---
+++ src/pages/Contact/type.ts
@@ -0,0 +1,23 @@
+export type contactCardType = {
+	github: {
+		mainImage: string;
+		mainImageDark: string;
+		description: string;
+		iconImage: string;
+		iconTitle: string;
+	};
+	blog: {
+		mainImage: string;
+		mainImageDark: string;
+		description: string;
+		iconImage: string;
+		iconTitle: string;
+	};
+	email: {
+		mainImage: string;
+		mainImageDark: string;
+		description: string;
+		iconImage: string;
+		iconTitle: string;
+	};
+};
